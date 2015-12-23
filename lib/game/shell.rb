8442feb22c17643817f111425c7fce367929a6e0
Pham Hoang Hiep <hoanghiepjp96@gmail.com>
2015-12-23 22:53:29 +0700

    fix shell merge problem

--- lib/game/shell.rb
+++ lib/game/shell.rb
@@ -2,7 +2,7 @@ module Chess
   class Shell
     attr_accessor :x, :y, :piece
 
-    def initialize(x, y, value = "")
+    def initialize(x, y, piece = Empty.new)
       @x = x
       @y = y
       @piece = piece
