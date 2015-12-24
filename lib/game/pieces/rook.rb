256b3b2889129bed85f725623396d008db4f3b3a
Zachary Perlmutter <zrp200@gmail.com>
2015-12-24 09:11:46 -0800

    made Rook a subclass of Piece

--- lib/game/pieces/rook.rb
+++ lib/game/pieces/rook.rb
@@ -1,11 +1,8 @@
-require "chess/pieces/pawn"
+require_relative "piece.rb"
 module Chess
-  class Rook < Pawn
+  class Rook < Piece 
     def initialize(color)
-      @value = case color.first.downcase
-        when ?b then "\u265C"
-        when ?w then "\u2656"
-      end
+      super color, "\u265C", "\u2656"
     end
   end
 end
