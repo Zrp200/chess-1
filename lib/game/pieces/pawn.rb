2bc26126975ddbb3ef10b8f56a8d9715ff9cd6ae
Zachary Perlmutter <zrp200@gmail.com>
2015-12-24 09:04:58 -0800

    Made Pawn a subclass of Piece

--- lib/game/pieces/pawn.rb
+++ lib/game/pieces/pawn.rb
@@ -1,16 +1,8 @@
+require_relative "piece.rb"
 module Chess
-  class Pawn
-    attr_reader :value, :moves
-
+  class Pawn < Piece
     def initialize(color)
-      @value = case color[0].downcase
-      when "w" then "\u{2659}"
-      when "b" then "\u{265F}"
-      end
-    end
-
-    def moves(x, y)
-      
+      super color, "\u265F", "\u2659"
     end
   end
 end
